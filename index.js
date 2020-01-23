0e82aab929e942b20c3a3bd74136701f724f8700
Sergei_Kundriukov <Sergei_Kundriukov@epam.com>
2020-01-23 22:59:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const postcss = require('postcss');
+
+const checkParentHasPrefix = (rule, prefix) => {
+  if (rule.parent.selector) {
+    return checkParentHasPrefix(rule.parent, prefix);
+  }
+
+  return rule.selector.includes(prefix);
+};
+
+module.exports = postcss.plugin('postcss-root-prefixer', (opts = {}) => {
+  const { prefix, fileNamePattern } = opts;
+  if (!prefix) {
+    throw new Error('Option "prefix" is missed!');
+  }
+
+  if (typeof prefix !== 'string') {
+    throw new Error('Option "prefix" must be a string!');
+  }
+
+  return (root, result) => {
+    const filePath = result.opts.to;
+
+    if (fileNamePattern && !fileNamePattern.test(filePath)) {
+      return;
+    }
+
+    root.walkRules(rule => {
+      if (rule.parent.selector && checkParentHasPrefix(rule, prefix)) {
+        return rule;
+      }
+
+      if (rule.selector) {
+        rule.selector = [prefix, rule.selector].join(' ');
+      }
+    });
+  };
+});
